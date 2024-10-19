6b3d88cfb05504061a9c0659ce808c354a59f640
DeZ <zhilindanilw@gmail.com>
2024-10-19 23:00:42 +0800

    Аннотация @Autowired

--- src/main/java/ru/spring/dez/MusicPlayer.java
+++ src/main/java/ru/spring/dez/MusicPlayer.java
@@ -1,38 +1,20 @@
 package ru.spring.dez;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MusicPlayer {
-    private Music music;
-    private String name;
-    private int volume;
+   private ClassicalMusic classicalMusic;
+   private RockMusic rockMusic;
 
-    public MusicPlayer() {
-    }
+   @Autowired
+   public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+       this.classicalMusic = classicalMusic;
+       this.rockMusic = rockMusic;
+   }
 
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public void playMusic() {
-        System.out.println("Playing: "+music.getSong());
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public String playMusic() {
+       return "Playing: " + classicalMusic.getSong();
     }
 }
--- src/main/java/ru/spring/dez/TestSpring.java
+++ src/main/java/ru/spring/dez/TestSpring.java
@@ -6,17 +6,8 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Music music = (Music) context.getBean("rockMusic", Music.class);
-
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-
-        musicPlayer.playMusic();
-
-        Music music2 = (Music) context.getBean("classicalMusic", Music.class);
-
-        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
-
-        classicalMusicPlayer.playMusic();
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
