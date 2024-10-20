63e9810ed653c9e76d635c7a29c4259547196d83
DeZ <zhilindanilw@gmail.com>
2024-10-20 19:06:16 +0800

    Конфигурация через джаву

--- src/main/java/ru/spring/dez/TestSpring.java
+++ src/main/java/ru/spring/dez/TestSpring.java
@@ -1,10 +1,13 @@
 package ru.spring.dez;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                SpringConfig.class
+        );
 
         MusicPlayer musicPlayer = (MusicPlayer) context.getBean("musicPlayer");
 
